594170630a13b24bd6c63567b5b2df4131bd4ecf
Brian Lee <brianleewj28@gmail.com>
2020-02-17 12:46:12 +0800

    added pausefunc during gameplay

---
+++ Assets/Scripts/Brian/PauseFunc.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseFunc : MonoBehaviour
+{
+    //if(PauseFunc.GamePaused)
+
+    public static bool GamePaused = false;
+
+    public GameObject PauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+                Resume();
+            else
+                Pause();
+        }
+    }
+
+    public void Resume()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit Game.");
+        Application.Quit();
+    }
+}
