d954beb8ff2454e1851da8f4f7fa27f78aab3682
ClaytonLoh <ClaytonLoh@users.noreply.github.com>
2020-02-18 15:57:57 +0800

    update player movement and add health

---
+++ Assets/Scripts/Clayton/HealthBarShrink.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarShrink : MonoBehaviour
+{
+    private const float DAMAGED_HEALTH_SHRINK_TIMER = 1f;
+    private Image barImage;
+    private Image damagedBarImage;
+    private float damagedHealthShrinkTimer;
+
+    private void Awake()
+    {
+        barImage = transform.Find("bar").GetComponent<Image>();
+        damagedBarImage = transform.Find("damagedBar").GetComponent<Image>();
+    }
+
+    private void SetHealth(float healthNormalized)
+    {
+        //barImage.fillAmount = healthNormalized;
+        PlayerPrefs.SetFloat("playerHealth", PlayerPrefs.GetFloat("playerHealth") - healthNormalized);
+        float temp = PlayerPrefs.GetFloat("playerHealth") / 100;
+        barImage.fillAmount = temp;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float temp = PlayerPrefs.GetFloat("playerHealth") / 100f;
+        barImage.fillAmount = temp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        damagedHealthShrinkTimer -= Time.deltaTime;
+        if (damagedHealthShrinkTimer < 0)
+        {
+            if (barImage.fillAmount < damagedBarImage.fillAmount)
+            {
+                float shrinkSpeed = 1f;
+                damagedBarImage.fillAmount -= shrinkSpeed * Time.deltaTime;
+            }
+        }
+
+        //Testing
+        //heal
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            SetHealth(80f);
+            damagedBarImage.fillAmount = barImage.fillAmount;
+        }
+
+        //Damage
+        if (Input.GetKeyDown(KeyCode.RightShift))
+        {
+            damagedHealthShrinkTimer = DAMAGED_HEALTH_SHRINK_TIMER;
+            SetHealth(50f);
+        }
+    }
+}
---
+++ Assets/Scripts/Clayton/PlayerCameraController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] private float lookSensitivity = 2f;
+    [SerializeField] private float smoothing = 2f;
+    private GameObject player;
+    private Vector2 smoothedVelocity;
+    private Vector2 currentLookingPos;
+
+    private void Start()
+    {
+        player = transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false; might use if have inventory
+    }
+
+    private void Update()
+    {
+        RotateCamera();
+    }
+
+    private void RotateCamera()
+    {
+        //Mouse X and Mouse Y (It is the default naming) can be found in Project settings -> Input
+        Vector2 inputValue = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        //Scaling input down
+        inputValue = Vector2.Scale(inputValue, new Vector2(lookSensitivity * smoothing, lookSensitivity * smoothing));
+
+        //Smoothing the motion. 
+        //Little optimisation: storing it as it is needed in every frame. Dont want to keep resetting it and getting a new one every frame
+        //Smoothing is like GetAxis instead of GetAxisRaw. I used smoothing as I can adjust the amount of smoothness I want the motion to be
+        smoothedVelocity.x = Mathf.Lerp(smoothedVelocity.x, inputValue.x, 1f / smoothing);
+        smoothedVelocity.y = Mathf.Lerp(smoothedVelocity.y, inputValue.y, 1f / smoothing);
+
+        //Take current vector pos and add a new one
+        currentLookingPos += smoothedVelocity;
+        currentLookingPos.y = Mathf.Clamp(currentLookingPos.y, -60f, 60f);
+
+        transform.localRotation = Quaternion.AngleAxis(-currentLookingPos.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(currentLookingPos.x, player.transform.up);
+    }
+}
