3447870bfb9120d400a4e1e82eaeed8e8979ec66
Brian Lee <brianleewj28@gmail.com>
2020-02-20 09:37:01 +0800

    added simple laser attack to enemy

---
+++ Assets/Scripts/Brian/EnemyLaser.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLaser : MonoBehaviour
+{
+    public GameObject laser;
+    private float cooldown = 5f;
+
+    EnemyStates states;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        states = GetComponent<EnemyStates>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldown -= Time.deltaTime;
+
+        if (states.currState == States.state_chase)
+        {
+            if (cooldown <= 0f)
+            {
+                GameObject las = Instantiate(laser, transform.position, transform.rotation);
+                las.GetComponent<Rigidbody>().velocity = transform.forward * 30;
+                cooldown = 5f;
+            }
+        }
+    }
+}
