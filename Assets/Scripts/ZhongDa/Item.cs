5a232b6760ef0ef661195cfda34e7e415850dc6d
Zackiee <42332559+Zackiee@users.noreply.github.com>
2020-02-21 14:30:36 +0800

    New Inventory

---
+++ Assets/Scripts/ZhongDa/Item.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int id;
+    public string type;
+    public string description;
+    public Sprite icon;
+    public bool pickedUp;
+
+    [HideInInspector]
+    public bool equipped;
+    [HideInInspector]
+    public GameObject weapon;
+    [HideInInspector]
+    public GameObject weaponManager;
+
+    public bool playerWeapon;
+
+    public void Start()
+    {
+        weaponManager = GameObject.FindGameObjectWithTag("WeaponManager");
+        if(!playerWeapon)
+        {
+            int allWeapons = weaponManager.transform.childCount;
+
+            for(int i = 0; i < allWeapons; i++)
+            {
+                if(weaponManager.transform.GetChild(i).gameObject.GetComponent<Item>().id == id)
+                {
+                    weapon = weaponManager.transform.GetChild(i).gameObject;
+                }
+            }
+        }
+    }
+
+    public void ItemUsage()
+    {
+        //weapon
+        if(type == "Weapon")
+        {
+            weapon.SetActive(true);
+            weapon.GetComponent<Item>().equipped = true;
+        }
+
+        //items
+    }
+
+    public void Update()
+    {
+        if(equipped)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+                equipped = false;
+            if (equipped == false)
+                this.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/ZhongDa/Slot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class Slot : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject item;
+    public int id;
+    public string type;
+    public string description;
+    public bool empty;
+
+    public Transform slotIconGo;
+    public Sprite icon;
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        UseItem();
+    }
+
+    private void Awake()
+    {
+        slotIconGo = transform.GetChild(0);
+    }
+    public void UpdateSlot()
+    {
+        slotIconGo.GetComponent<Image>().sprite = icon;
+    }
+
+    public void UseItem()
+    {
+        item.GetComponent<Item>().ItemUsage();
+    }
+}
