6a897584017322340ac69f76956cc9dec1461b4a
Wee Hong Ru Curtis <curtisw2e@yahoo.com.sg>
2020-02-21 16:03:21 +0800

    updated weapon manager for ease of management
    also added code for following camera

--- Assets/Scripts/Curtis/WeaponManager.cs
+++ Assets/Scripts/Curtis/WeaponManager.cs
@@ -5,25 +5,34 @@ using UnityEngine;
 public class WeaponManager : MonoBehaviour
 {
     public List<GameObject> weapons;
-    private float scroll;
 
-    public int currWeapon;
+    private float scroll;
+    private int currWeapon;
+    private int prevWeapon;
+
+    [SerializeField]
+    private Transform camera;
 
     private void Start()
     {
+        camera = transform.parent.GetChild(0);
         for(int i = 0; i < transform.childCount; ++i)
         {
             weapons.Add(transform.GetChild(i).gameObject);
         }
 
         currWeapon = 0;
-        weapons[0].SetActive(true);
-        weapons[1].SetActive(false);
+        //weapons[0].SetActive(true);
+        //weapons[1].SetActive(false);
     }
 
     private void Update()
     {
+        transform.rotation = camera.rotation;
+
         scroll = Input.GetAxis("Mouse ScrollWheel") / 0.1f;
+        prevWeapon = currWeapon;
+        currWeapon += (int)scroll;
 
         if (currWeapon < 0)
         {
@@ -34,21 +43,6 @@ public class WeaponManager : MonoBehaviour
             currWeapon = 0;
         }
 
-        currWeapon += (int)scroll;
-
-        switch (currWeapon)
-        {
-            case 0:
-                weapons[0].SetActive(true);
-                weapons[1].SetActive(false);
-                break;
-            case 1:
-                weapons[1].SetActive(true);
-                weapons[0].SetActive(false);
-                break;
-        }
-
-
         if (Input.GetKeyDown("1"))
         {
             currWeapon = 0;
@@ -57,5 +51,28 @@ public class WeaponManager : MonoBehaviour
         {
             currWeapon = 1;
         }
+
+        if (currWeapon != prevWeapon)
+        {
+            Debug.Log("change");
+            foreach (GameObject weapon in weapons)
+            {
+                weapon.SetActive(false);
+            }
+            weapons[currWeapon].SetActive(true);
+        }
+
+
+        //switch (currWeapon)
+        //{
+        //    case 0:
+        //        weapons[0].SetActive(true);
+        //        weapons[1].SetActive(false);
+        //        break;
+        //    case 1:
+        //        weapons[1].SetActive(true);
+        //        weapons[0].SetActive(false);
+        //        break;
+        //}
     }
 }
