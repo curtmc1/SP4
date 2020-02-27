00940c1c80e13fd31ccdbe11ad7072357354098f
Brian Lee <brianleewj28@gmail.com>
2020-02-27 11:19:15 +0800

    made sure music volume is using saved value at menu
    made sure enemy head stays on enemy body

--- Assets/Scripts/Brian/Menu.cs
+++ Assets/Scripts/Brian/Menu.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class Menu : MonoBehaviour
 {
+    public AudioMixer musicMixer;
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerPrefs.SetString("LevelName", "Level1");
+        musicMixer.SetFloat("MusicVolume", PlayerPrefs.GetFloat("MusicVolume"));
     }
 
     public void Play()
