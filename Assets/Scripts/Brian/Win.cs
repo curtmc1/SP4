0166f4651135a0e823ec860f6d2fd0afe503d12d
Brian Lee <brianleewj28@gmail.com>
2020-03-04 19:04:26 +0800

    added win condition

---
+++ Assets/Scripts/Brian/Win.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    public GameObject WinMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.GetComponentInParent<CheckWin>().reachedEnd)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            WinMenu.SetActive(true);
+        }
+    }
+
+    public void LoadMenu()
+    {
+        gameObject.GetComponentInParent<CheckWin>().reachedEnd = false;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit Game.");
+        Application.Quit();
+    }
+}
